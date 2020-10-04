07cc446066867fef1e4bef51de55d88cbfed58a3
imd10tran <64759690+imd10tran@users.noreply.github.com>
2020-10-04 14:24:38 -0400

    Update LinkedListSum.java

--- Part 2/LinkedListSum.java
+++ Part 2/LinkedListSum.java
@@ -1,14 +1,22 @@
 import java.util.LinkedList;
 
 public class LinkedListSum{
-
 	public static double summingLinkedList(){
-		LinkedList<Character> filledLinkedList = FillingLists.fillingLinkedList();
+		String alphabet = Size.alphabet;
+		int numberOfElements = 1000000;
+		System.out.println("start");
+		LinkedList<Character> filledLinkedList = new LinkedList<Character>();
+		for(int i = 0; i<numberOfElements; i++){
+			char c = (char) alphabet.charAt(i%26);
+			filledLinkedList.add(c);}
+		
 		String sum = "";
 		long start = System.currentTimeMillis();
-		for(int i = 0; i<Size.getNumberOfElements(); i++){
-			sum+=filledLinkedList.get(i);
+		for(int i = 0; i<numberOfElements; i++){
+			sum=sum+filledLinkedList.get(i);
+			
 		}
+		
 		long stop = System.currentTimeMillis();
 		double timeElapsed = (double) stop - start; 
 		return timeElapsed;
